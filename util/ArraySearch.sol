bd752706fec60335aff5015312f18f756c49c67b
Joan Jimenez Jané <jimenezxik91@gmail.com>
2023-04-19 13:56:50 +0200

    Create ArraySearch.sol

---
+++ util/ArraySearch.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+// JimenezXik Contracts (last updated v1.0.0) (utils/ArraySearch.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Collection of functions related to array search.
+ */
+library ArraySearch {
+    /**
+    * @dev Implementation of binary search to find the index of a given number in a sorted array of numbers sorted Ascendant
+    *
+    * `sortedArray` is the array which we are going to search the element.
+    * `elem` is the value to find in the array.
+    *
+    * Returns de position where the value has to been inserted.
+    * If the function reaches the end of the while loop without finding the number, it returns -1 to indicate that the number was not found.
+    */
+    function binarySearchAsc(uint256[] memory sortedArray, uint256 elem) internal pure returns (int256) {
+        int256 left = 0;
+        int256 right = int256(sortedArray.length - 1);
+        
+        while (left <= right) {
+            int256 mid = left + (right - left) / 2;
+            if (sortedArray[uint256(mid)] == elem) {
+                return mid;
+            } else if (sortedArray[uint256(mid)] < elem) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+
+    /**
+    * @dev Implementation of binary search to find the index of a given number in a sorted array of numbers sorted Descendant
+    *
+    * `sortedArray` is the array which we are going to search the element.
+    * `elem` is the value to find in the array.
+    *
+    * Returns de position where the value has to been inserted.
+    * If the function reaches the end of the while loop without finding the number, it returns -1 to indicate that the number was not found.
+    */
+    function binarySearchDesc(uint256[] memory sortedArray, uint256 elem) internal pure returns (int256) {
+        int256 left = 0;
+        int256 right = int256(sortedArray.length - 1);
+        
+        while (left <= right) {
+            int256 mid = left + (right - left) / 2;
+            if (sortedArray[uint(mid)] == elem) {
+                return mid;
+            } else if (sortedArray[uint(mid)] < elem) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
