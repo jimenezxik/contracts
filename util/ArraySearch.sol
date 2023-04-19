60e264e54a8a804d8c302f75a0939b68b8afacec
Joan Jimenez Jané <jimenezxik91@gmail.com>
2023-04-19 16:36:33 +0200

    Update ArraySearch.sol

--- util/ArraySearch.sol
+++ util/ArraySearch.sol
@@ -49,9 +49,9 @@ library ArraySearch {
         
         while (left <= right) {
             int256 mid = left + (right - left) / 2;
-            if (sortedArray[uint(mid)] == elem) {
+            if (sortedArray[uint256(mid)] == elem) {
                 return mid;
-            } else if (sortedArray[uint(mid)] < elem) {
+            } else if (sortedArray[uint256(mid)] < elem) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
@@ -60,4 +60,42 @@ library ArraySearch {
         
         return -1;
     }
+
+    /**
+    * @dev Searches for the first occurrence of a given number in an array from the beginning to the end of the array
+    *
+    * `array` is the array which we are going to search the element.
+    * `elem` is the value to find in the array.
+    *
+    * Returns de position where the value has to been inserted.
+    * If the function reaches the end of the while loop without finding the number, it returns -1 to indicate that the number was not found.
+    */
+    function firstOccurrenceStartToEnd(uint256[] memory array, uint256 elem) internal pure returns (int256) {
+        for (uint256 i = 0; i < array.length; i++) {
+            if (array[i] == elem) {
+                return int256(i);
+            }
+        }
+        
+        return -1;
+    }    
+
+    /**
+    * @dev Searches for the first occurrence of a given number in an array from the end to the beggining of the array
+    *
+    * `array` is the array which we are going to search the element.
+    * `elem` is the value to find in the array.
+    *
+    * Returns de position where the value has to been inserted.
+    * If the function reaches the end of the while loop without finding the number, it returns -1 to indicate that the number was not found.
+    */
+    function firstOccurrenceEndToStart(uint256[] memory array, uint256 elem) internal pure returns (int256) {
+        for (int256 i = int256(array.length) - 1; i >= 0; i--) {
+            if (array[uint256(i)] == elem) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }    
 }
